405ef1115414bd580e32173a8c4f1523c10de1f1
Dariusz Jedralski <djedralski@luc.edu>
2017-11-29 00:44:38 -0600

    Added a few pakckages and have database connected for now

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var app = express();
+var http = require("http");
 var port = 3000;
 var Game = require("./game.js");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var userinfoTotal = {};
+
+
+mongoose.connect('mongodb://appUser:password33!@ds119446.mlab.com:19446/connect4');
+
+// Configuring Passport
+var passport = require('passport');
+var expressSession = require('express-session');
+app.use(expressSession({ secret: 'mySecretKey' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 //defines the first player, the one who waits for the second one to connect
 var waitingPlayer = null;
@@ -12,65 +26,64 @@ app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
 
 var Player = function(client) {
-	this.client = client;
+    this.client = client;
 };
 
 //when a player connects
-io.sockets.on('connection', function (socket) {
-	var player = new Player(socket);
+io.sockets.on('connection', function(socket) {
+    var player = new Player(socket);
 
-  //sends a new move to the other player
-  var sendMove = function(col){
-    console.log("send move ", col);
-    player.game.currentPlayer.client.json.send({move: col});
-  };
+    //sends a new move to the other player
+    var sendMove = function(col) {
+        console.log("send move ", col);
+        player.game.currentPlayer.client.json.send({ move: col });
+    };
 
-  //in case of win, sends message to each player with the adequate message
-  var sendWin = function(winner) {
-    var firstPlayerResult,
-        secondPlayerResult;
+    //in case of win, sends message to each player with the adequate message
+    var sendWin = function(winner) {
+        var firstPlayerResult,
+            secondPlayerResult;
 
-    if (winner == 0) {
-      firstPlayerResult = secondPlayerResult = 'tie';
+        if (winner == 0) {
+            firstPlayerResult = secondPlayerResult = 'tie';
+        } else {
+            var firstPlayerIsWinner = (winner == firstPlayer);
+            firstPlayerResult = (firstPlayerIsWinner) ? "win" : "lost";
+            secondPlayerResult = (firstPlayerIsWinner) ? "lost" : "win";
+        }
+
+        firstPlayer.client.json.send({ win: firstPlayerResult });
+        secondPlayer.client.json.send({ win: secondPlayerResult });
     }
+
+
+    //the case when the first player is connected, and waits for the second
+    if (waitingPlayer == null) {
+        waitingPlayer = player;
+    }
+    //the second player connects,
+    //waitingPlayer becomes null, so another first player can start another game
     else {
-      var firstPlayerIsWinner = (winner == firstPlayer);
-      firstPlayerResult = (firstPlayerIsWinner) ? "win" : "lost";
-      secondPlayerResult = (firstPlayerIsWinner) ? "lost" : "win";
+        var secondPlayer = player;
+        var firstPlayer = waitingPlayer;
+        waitingPlayer = null;
+
+        //sending order of turns to connected users
+        firstPlayer.client.json.send({ turn: 1 });
+        secondPlayer.client.json.send({ turn: 2 });
+
+        //creating a new game, and passing created palyers and functions
+        var game = new Game();
+        game.create(firstPlayer, secondPlayer, sendMove, sendWin);
+
+        //sending each player information about the game
+        firstPlayer.game = game;
+        secondPlayer.game = game;
     }
-
-    firstPlayer.client.json.send({win : firstPlayerResult});
-    secondPlayer.client.json.send({win : secondPlayerResult});
-  }
-
-
-  //the case when the first player is connected, and waits for the second
-  if (waitingPlayer == null) {
-		waitingPlayer = player;
-	}
-  //the second player connects,
-  //waitingPlayer becomes null, so another first player can start another game
-	else {
-		var secondPlayer = player;
-		var firstPlayer = waitingPlayer;
-		waitingPlayer = null;
-
-    //sending order of turns to connected users
-		firstPlayer.client.json.send({turn: 1});
-		secondPlayer.client.json.send({turn: 2});
-
-    //creating a new game, and passing created palyers and functions
-    var game = new Game();
-    game.create(firstPlayer, secondPlayer, sendMove, sendWin);
-
-    //sending each player information about the game
-		firstPlayer.game = game;
-		secondPlayer.game = game;
-	}
-  //socket waits for the message 'submit-move' to process the sent move
-	socket.on('submit-move', function(data){
-	   console.log('submit-move: ', data);
-	    player.game.onMove(data.move);
-	});
+    //socket waits for the message 'submit-move' to process the sent move
+    socket.on('submit-move', function(data) {
+        console.log('submit-move: ', data);
+        player.game.onMove(data.move);
+    });
 
 });
