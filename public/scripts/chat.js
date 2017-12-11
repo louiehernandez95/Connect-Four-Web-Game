a35815f2324355b16e190568d8f557e927303453
Dariusz Jedralski <djedralski@gmail.com>
2017-12-11 00:14:43 -0600

    added change to include name automatically created in chat

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,14 +1,24 @@
 var socket = io.connect('http://localhost:3000');
 
-function addMessage (data) {
+function addMessage(data) {
     $('#messages').prepend('<li class="list-group-item">' +
         '<h4 class="list-group-item-heading">' + data.name + '</h4>' +
         '<p class="list-group-item-text">' + data.message + '</p>' +
-    '</li>');
+        '</li>');
 };
 
+var usersName = "";
+
+fetch('/name', { credentials: "same-origin" }).then(function(response) {
+    console.log("print fetch name process....:")
+
+    response.text().then(function(text) {
+        usersName = text;
+    });
+});
+
 // This will be fired 
-socket.on('messages-available', function (data) {
+socket.on('messages-available', function(data) {
     for (var i = 0; i < data.length; i++) {
         addMessage(data[i]);
     }
@@ -18,14 +28,14 @@ socket.on('messages-available', function (data) {
 socket.on('message-added', addMessage);
 
 // When someone clicks the "Send" button, we'll emit the data to the server
-$('#create-message').submit(function (e) {
+$('#create-message').submit(function(e) {
 
     // Don't let the form actually post to the server
     e.preventDefault();
-    
+
     // Send the "add-message" message to the server with our values
     socket.emit('add-message', {
-        name: $('input[name="name"]').val(),
+        name: usersName,
         message: $('textarea[name="message"]').val()
     });
 
--- router.js
+++ router.js
@@ -43,7 +43,7 @@ router.post('/', function(req, res, next) {
                 var err = new Error('Wrong email or password.');
                 err.status = 401;
                 return next(err);
-            
+
             } else {
                 req.session.userId = user._id;
                 return res.redirect('/game');
@@ -121,24 +121,6 @@ router.post('/signup', function(req, res, next) {
     // return res.sendFile('html/index.html', { root: __dirname });
 });
 
-// GET route after registering
-router.get('/profile', function(req, res, next) {
-    User.findById(req.session.userId)
-        .exec(function(error, user) {
-            if (error) {
-                return next(error);
-            } else {
-                if (user === null) {
-                    var err = new Error('Not authorized! Go back!');
-                    err.status = 400;
-                    return next(err);
-                } else {
-                    return res.send('<h1>Name: </h1>' + user.username + '<h2>Mail: </h2>' + user.email + '<br><a type="button" href="/logout">Logout</a>')
-                }
-            }
-        });
-});
-
 
 router.get('/name', function(req, res, next) {
     User.findById(req.session.userId)
@@ -177,7 +159,7 @@ router.get("/info.json", function(req, res) {
         //add some error checking...
         res.json(info);
         // console.log(res.);
-    }).sort({"wins":-1, "losses":1});
+    }).sort({ "wins": -1, "losses": 1 });
 });
 
 
